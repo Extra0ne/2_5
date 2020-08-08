2a9dac6bfdd3f0bf083235a22c0e8555b8f9a5e6
Vasya Bogatchuk <v380962437840@gmail.com>
2020-08-08 15:32:12 +0300

    no message

--- 2_5/Program.cs
+++ 2_5/Program.cs
@@ -8,23 +8,22 @@ namespace _2_5
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
             Console.Write("Введите число :");
             int num = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите степень :");
             int power = Convert.ToInt32(Console.ReadLine());
-            int result=0;
-                for (int i = 1; i < power; i++)
-                {
-                    result = num * num;
-                }
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= num ;
+            }
             Console.WriteLine("Результат = {0}", result);
 
             Console.ReadLine();
-
-
-
 
         }
     }
